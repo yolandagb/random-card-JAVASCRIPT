28822b86e0468ecd8a4432fdee2ba4e83f4c6ea8
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-04 17:09:26 +0000

    detalles que repasar, no se ve en el buscador

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("estoy harta de gitpod");
-};
+function main() {
+  document.getElementById("Boton").addEventListener("click", changeCard);
+  //console.log("estoy harta de gitpod");
+}
+function changeCard() {
+  let poker = ["heart", "spades", "club", "diamond"];
+  let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+}
+let numberRandom = Math.floor(Math.random() * card.length);
+console.log(card[numberRandom]);
+
+let elementCard = document.querySelector(".Card");
+let elementNumber = document.querySelector(".Number");
+elementNumber.innerHTML = card[numberRandom];
+
+let randomPoker = poker[Math.floor(Math.random() * poker.length)];
+elementCard.className = "theCard";
+elementCard.classList.add(randomPoker);
+
+window.addEventListener("load", () => main());
