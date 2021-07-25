180193b82115a9c04e1d68515d4e91b999e24e88
yolandagb <yolandagarciabarranco@hotmail.com>
2021-07-25 12:12:06 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,22 +6,21 @@ import "./style.css";
 // import "./assets/img/4geeks.ico";
 
 function main() {
-  document.getElementById("Boton").addEventListener("click", changeCard);
-  //console.log("estoy harta de gitpod");
+  document.getElementById("boton").addEventListener("click", changeCard);
 }
 function changeCard() {
   let poker = ["heart", "spades", "club", "diamond"];
   let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let numberRandom = Math.floor(Math.random() * card.length);
+  console.log(card[numberRandom]);
+
+  let elementCard = document.querySelector(".Card");
+  let elementNumber = document.querySelector(".Number");
+  elementNumber.innerHTML = card[numberRandom];
+
+  let randomPoker = poker[Math.floor(Math.random() * poker.length)];
+  elementCard.className = "Card";
+  elementCard.classList.add(randomPoker);
 }
-let numberRandom = Math.floor(Math.random() * card.length);
-console.log(card[numberRandom]);
-
-let elementCard = document.querySelector(".Card");
-let elementNumber = document.querySelector(".Number");
-elementNumber.innerHTML = card[numberRandom];
-
-let randomPoker = poker[Math.floor(Math.random() * poker.length)];
-elementCard.className = "Card";
-elementCard.classList.add(randomPoker);
-
 window.addEventListener("load", () => main());
